0eb2943dee22306ca7a1f7d3dde29f1acc432997
Antoine Roy-Gobeil <antoine@plot.ly>
2019-02-08 17:47:31 -0500

    initial commit

---
+++ johnson.js
@@ -0,0 +1,146 @@
+const tarjan = require("strongly-connected-components");
+
+module.exports = function findCircuits(edges) {
+    var circuits = []; // Output
+
+    var stack = [];
+    var blocked = [];
+    var B = {};
+    var Ak = [];
+    var s;
+
+    function unblock(u) {
+        blocked[u] = false;
+        for (var i = 0, last=B[u].length; i < last; i++) {
+            var w = B[u][i];
+            B[u].splice(i, 1);
+            if (blocked[w]) { unblock(w); }
+        }
+    }
+
+    function circuit(v) {
+        var found = false;
+
+        stack.push(v);
+        blocked[v] = true;
+
+        // L1
+        for (var i = 0; i < Ak[v].length; i++) {
+            var w = Ak[v][i];
+            if (w === s) {
+              output(s, stack);
+              found = true;
+            } else if (!blocked[w]) {
+              found = circuit(w);
+            }
+        }
+
+        // L2
+        if (found) {
+          unblock(v);
+        } else {
+          for (i = 0; i < Ak[v].length; i++) {
+              w = Ak[v][i];
+              if (B[w].indexOf(v) === -1) {
+                B[w].push(v);
+              }
+          }
+        }
+        stack.pop();
+        return found;
+    }
+
+    function output(start, stack) {
+        var cycle = [].concat(stack).concat(start);
+        circuits.push(cycle);
+    }
+
+    function subgraph(minId) {
+      // Remove edges with indice smaller than minId
+      for (var i = 0; i < edges.length; i++) {
+        if (i < minId) edges[i] = [];
+        edges[i] = edges[i].filter(function(i) {
+          return i >= minId;
+        });
+      }
+      // var subgraph = listOfEdges.map(function(l, index) {
+      //   if (index < minId) return [];
+      //
+      //   return l.filter(function(i) {
+      //     return i >= minId;
+      //   }) || [];
+      // });
+      //
+      // return subgraph;
+    }
+
+    function adjacencyStructureSCC(from) {
+      // Make subgraph starting from vertex minId
+      subgraph(from);
+      var g = edges
+
+      // Find strongly connected components using Tarjan algorithm
+      var sccs = tarjan(g);
+
+      // Filter out trivial connected components (ie. made of one node)
+      var ccs = sccs.components.filter(function(scc) {
+          return scc.length > 1;
+      });
+
+      // Find least vertex
+      var leastVertex = Infinity;
+      var leastVertexComponent;
+      for (var i = 0; i < ccs.length; i++) {
+        for (var j = 0; j < ccs[i].length; j++) {
+          if (ccs[i][j] < leastVertex) {
+            leastVertex = ccs[i][j];
+            leastVertexComponent = i;
+          }
+        }
+      }
+
+      var cc = ccs[leastVertexComponent];
+
+      if (!cc) return false;
+
+      // Return the adjacency list of first component
+      var adjList = edges.map(function(l, index) {
+        if (cc.indexOf(index) === -1) return [];
+        return l.filter(function(i) {
+          return cc.indexOf(i) !== -1;
+        });
+      });
+
+      return [leastVertex, adjList];
+    }
+
+    s = 0;
+    var n = edges.length;
+    while (s < n) {
+        // find strong component with least vertex in
+        // subgraph starting from vertex `s`
+        var p = adjacencyStructureSCC(s);
+
+        // Its least vertex
+        s = p[0];
+        // Its adjacency list
+        Ak = p[1];
+
+        if (Ak) {
+          for (var i = 0; i < Ak.length; i++) {
+            for (var j = 0; j < Ak[i].length; j++) {
+              var v_id = Ak[i][j];
+              blocked[+v_id] = false;
+              B[v_id] = [];
+            }
+          }
+          circuit(s);
+          s = s + 1;
+        } else {
+          s = n;
+        }
+
+    }
+
+    return circuits;
+}
---
+++ test/test.js
@@ -0,0 +1,67 @@
+"use strict"
+
+var findCircuits = require("../johnson.js");
+
+var test = require('tape');
+
+test("find elementarty circuits in a directed graph", function(t) {
+  t.test('simple directed graph', function(t) {
+      var g = [
+        [4], // 0
+        [0,2], // 1
+        [1,3], // 2
+        [2], // 3
+        [1], // 4
+        [4,6], // 5
+        [5,2], // 6
+        [7,6,3], // 7
+      ]
+
+      var circuits = findCircuits(g);
+      t.equal(circuits.length, 4);
+      t.end();
+  })
+
+  t.test('another simple directed graph', function(t) {
+    //   V4      V2
+    // +-<---o---<---o---<--+
+    // |             |      |
+    // V0 o             ^      o V3
+    // |           V1|      |
+    // +------>------o--->--+
+
+
+    var g = [
+      [1],
+      [2, 3],
+      [4],
+      [2],
+      [0]
+    ]
+
+    var circuits = findCircuits(g);
+    t.deepEqual(circuits, [[0, 1, 2, 4, 0], [0, 1, 3, 2, 4, 0]]);
+    t.end();
+  });
+
+  t.test('it scales with the number of nodes and edges', function(t) {
+    var N = 100;
+    var L = 4;
+    var g = []
+    for (var i = 0; i < N; i++) {
+      g[i] = [];
+      for (var j = 0; j < L; j++) {
+        var target = Math.floor(Math.random() * N);
+        if (g[i].indexOf(target) === -1) g[i].push(target);
+      }
+    }
+
+    var circuits = findCircuits(g);
+    console.log(circuits);
+    t.end();
+  })
+
+
+
+  t.end()
+})
